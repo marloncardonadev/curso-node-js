ece80efdfafe699d83f7ab1cb97737eb3fe144d2
Marlon Cardona <marlon18_@hotmail.com>
2023-10-01 23:47:40 -0500

    Commit finaliza clase-3

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const crypto = require('node:crypto')
-const movies = require('./movies.json')
+const cors = require('cors')
 
+const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234',
+      'https://movies.com'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
 app.disable('x-powered-by')
 
 app.get('/movies', (req, res) => {
@@ -42,6 +62,19 @@ app.post('/movies', (req, res) => {
   movies.push(newMovie)
 
   res.status(201).json(newMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
 })
 
 app.patch('/movies/:id', (req, res) => {
